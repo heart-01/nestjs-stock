32cb7d22765c6694dfb0324e248529f06578da9a
Siwat Jermwatthana <s1104300051612@gmail.com>
2023-11-21 11:13:22 +0700

    [Feat]: Add helmet middleware for setting HTTP response headers

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 import * as express from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
 
+  // Helmet middleware for setting HTTP response headers.
+  app.use(helmet());
+
+  // Cors middleware for allowing cross-origin resource sharing.
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: 'GET,PATCH,POST,DELETE',
@@ -19,7 +24,15 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     preflightContinue: false,
   });
+
+  // Middleware for URL-encoded form data.
+  app.use(express.urlencoded({ extended: true }));
+
+  // Middleware for JSON data.
   app.use(express.json({ limit: '10mb' }));
+
+  // Middleware for serving static files.
+  app.use(express.static('public'));
 
   await app.listen(process.env.PORT || 3000);
 }
